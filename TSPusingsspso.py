28dd93bdba267cf9bf7e360467c63fe8011bc39a
SudipMahajan <42032637+SudipMahajan@users.noreply.github.com>
2018-10-16 08:38:37 +0530

    Add files via upload

---
+++ TSPusingsspso.py
@@ -0,0 +1,84 @@
+import numpy as np
+import psofortspmodule as PSO
+
+'''
+TSP problem is to find minimum cost Hamiltonian Cycle in the graph
+All functions and TSP graph are initialized in user defined module 'psofortspmodule.py'
+This code assumes TSP graph to be a complete graph, so while calculating TSP tour, it is understood that
+last node is same as first, so last node is NOT considered explicitly
+'''
+
+############################################### START OF CODE #######################################
+
+## STEP 1: Initialization
+print ("TSP graph created...")
+
+listofparticles = PSO.initializeparticles(n_particles = 100)
+print ("Particles initialized to compute TSP tour")
+# PSO.displayparticles(listofparticles)
+GlobalBest = PSO.computeG(listofparticles)
+print ("\n ******Initial Global Best tour:",GlobalBest["tour"], ",with cost :",GlobalBest["cost"],"****** \n")
+
+n_iter = 10
+
+## STEP 2
+for iter in range(n_iter):
+
+	iterglobal = {}    # dictionary to store best tour path and cost every iteration
+	print ("\nIteration:",iter)
+## STEP 3
+	for particle in listofparticles:   # For all particles 
+	
+
+		# COMPUTE A = Pid (Previous best tour found by a particle) - X (Current tour position of particle)
+		A = PSO.tourpositionsubtract( particle["bestpos"].copy(), particle["currpos"].copy() )
+		assert ( PSO.SS( particle["currpos"].copy(), A ) == particle["bestpos"].copy() )      # cross checking
+
+		# COMPUTE B = Gid (Previous best achieved by any particle) - X (Current tour position)
+		B = PSO.tourpositionsubtract( GlobalBest["tour"].copy(), particle["currpos"].copy() )
+		assert ( PSO.SS( particle["currpos"].copy(), B ) == GlobalBest["tour"].copy() )      # cross checking
+
+		# Calculate new Velocity Vid = Vid (Previous iteration velocity) + alpha * A + beta * B; 
+		# where alpha & beta are randomly generated values between zero and one, '+' is concatenation operator implemented by 'extend'
+		alpha = np.random.random()
+		beta = np.random.random()
+		if ( alpha > 0.7 ):
+			particle["vel"].extend(A)   
+		if ( beta > 0.7 ):
+			particle["vel"].extend(B)
+
+
+		# Calculate new current tour Position using Xid = Xid + Vid and corresponding cost
+		particle["currpos"] = PSO.SS( particle["currpos"] , particle["vel"] )
+		particle["currcost"] = PSO.calcTourCost( particle["currpos"] )
+		assert ( particle["currcost"] == PSO.calcTourCost( particle["currpos"] ) ),( particle["currcost"], PSO.calcTourCost( particle["currpos"]) )    # cross checking code
+
+		# Implement partial search to compute new position of particle, VTPSO paper implementation
+		particle["currpos"], particle["currcost"] = PSO.partialsearch(particle["currpos"],particle["vel"])
+
+
+		# Update 'P'i.e best tour for particle if current tour is better than stored best tour
+		if( particle["currcost"] < particle["bestcost"] ):
+			particle["bestpos"] = particle["currpos"]
+		
+		particle["bestcost"] = PSO.calcTourCost( particle["bestpos"] )
+
+		assert ( particle["bestcost"] == PSO.calcTourCost( particle["bestpos"] ) ),particle    # cross checking code
+
+    # inner FOR loop ends, updating particle postions and velocities is over
+
+	# PSO.displayparticles(listofparticles)
+	iterglobal =  PSO.computeG(listofparticles)
+	print ( "\n** Global Best Tour:",GlobalBest["tour"],"with Cost:",GlobalBest["cost"] )
+	assert ( iterglobal["cost"] == PSO.calcTourCost( iterglobal["tour"] ))    # cross checking code
+
+## STEP 4 
+# Update Global tour if required
+	if ( iterglobal["cost"] < GlobalBest["cost"] ):
+		GlobalBest = iterglobal.copy()
+
+	assert ( GlobalBest["cost"] <= iterglobal["cost"] ),( GlobalBest,iterglobal )   # cross checking code
+### End of outer FOR Loop, represents ITERATIONS
+
+print ("\n\n After all iterations:\n Global Best tour",GlobalBest["tour"],"with cost:",GlobalBest["cost"])
+############################################# END OF CODE ################################################
---
+++ psofortspmodule.py
@@ -0,0 +1,128 @@
+import random as rm
+
+
+# Initialize graph of cities as adjacency matrix
+'''
+graphofcities = [[0, 2, 2, 5, 7],
+                 [2, 0, 4, 1, 2],
+                 [2, 4, 0, 1, 3],
+                 [5, 1, 1, 0, 2],
+                 [7, 2, 3, 2, 0]]
+                 # Optimum tour: 0-1-4-3-2-0 cost: 9
+
+'''
+
+graphofcities = [[0, 10, 24,  7,  6, 12, 12,  9, 23,  1],
+                 [10, 0, 17, 12,  5, 14,  7, 22,  2, 24],
+                 [24, 17, 0, 23, 23,  4,  7, 19, 20, 25],
+                 [ 7, 12, 23, 0, 18, 10, 15, 14,  2,  7],
+                 [ 6,  5, 23, 18, 0, 10, 18,  8,  2,  6],
+                 [12, 14,  4, 10, 10, 0,  8,  6, 14, 20],
+                 [12,  7,  7, 15, 18,  8, 0, 18,  7,  5],
+                 [ 9, 22, 19, 14,  8,  6, 18, 0, 10,  2],
+                 [23,  2, 20,  2,  2, 14,  7, 10, 0, 15],
+                 [ 1, 24, 25,  7,  6, 20,  5,  2, 15, 0]]
+                 # optimum tour cost: 43
+
+
+def calcTourCost(toursequence):
+	tourcost = 0
+
+	for i in range(len(toursequence)-1):
+		# print (graphofcities[ toursequence[i] ][ toursequence[i+1] ])
+		tourcost = tourcost + graphofcities[ toursequence[i] ][ toursequence[i+1] ]
+
+	tourcost = tourcost + graphofcities[ toursequence[-1] ][ toursequence[0] ]          # to complete the cycle
+	return tourcost
+
+
+def SO(list2swap,swaptuple):   # swap operator
+	'''
+	'swapindextuple' is the tuple of indices to swap from list2swap
+	'''
+	list2swap[swaptuple[0]], list2swap[swaptuple[1]] = list2swap[swaptuple[1]], list2swap[swaptuple[0]]
+	return list2swap
+
+
+def SS(tlist,SOlist):
+	'''
+	Applies 'plus' operation 
+	'SOlist' is the list of swap operator tuples
+	'''
+	for SOtuple in SOlist:
+		tlist = SO(tlist, SOtuple)
+		#print ("after apply first SO",SOtuple,"above list:",tlist)
+
+	return tlist
+
+
+
+def initializeparticles(n_particles):
+	'''
+	Each particle is a potential solution in the form of a tuple of 3 lists.
+	The 1st included list is for position (a tour around the cities)
+	The 2nd list is for velocity (Swap Sequence)i.e. list of tuples
+	The 3rd included list is for best tour sequence achived by that particle in all previous iterations.
+	'''
+	n_cities = len(graphofcities[0])
+	print ("Number of cities:",n_cities)
+	listofparticles = []
+	for i in range(n_particles):                                  
+		pos = rm.sample(range(0,n_cities),n_cities)      # initialize random position
+		bestpos = rm.sample(range(0,n_cities),n_cities)  # initialize random best position for the particle
+		
+		vel = []
+		for i in range(4):                                    # initialize random velocity sequence for every particle
+			vel.append( (rm.choice(range(0,n_cities)), rm.choice(range(0,n_cities)) ) )
+		listofparticles.append( {"currpos":pos,"currcost":calcTourCost(pos),"vel":vel,"bestpos":bestpos,"bestcost":calcTourCost(bestpos) } )
+	return listofparticles
+
+
+def displayparticles(listofparticles):
+	for i in range(len(listofparticles)):
+		print ("\nParticle ",i,": Position:",listofparticles[i]["currpos"], "current tour cost:",listofparticles[i]["currcost"],"\nTour Best:",listofparticles[i]["bestpos"], "Cost of Tour Best:", listofparticles[i]["bestcost"])
+
+	return
+
+def tourpositionsubtract(tourpos1, tourpos2):
+	'''
+	Applies '-' i.e. subtraction operator. This function computes edit distance
+	Finds a Basic swap sequence which applied on tourpos2 will give tourpos1
+	'''
+	toursubtractSS = []
+	for i in range(len(tourpos1)):
+		pos = tourpos2.index( tourpos1[i] )
+
+		if (i != pos):
+			swapop = ( i,pos )
+			toursubtractSS.append( swapop )
+			tourpos2 = SO(tourpos2,swapop)
+
+	return toursubtractSS
+
+def computeG(listofparticles):
+	seq = [ (x["bestpos"],x["bestcost"]) for x in listofparticles ] 
+	tour, cost = min( seq, key = lambda t:t[1] )
+	Best = {"tour":tour,"cost":cost}
+	assert ( Best["cost"] == calcTourCost( Best["tour"] ))    # cross checking code
+	return Best
+
+
+def partialsearch(tourpos, vel):
+	min_cost = 1000
+	cost = 0
+	min_cost_tour = []
+
+
+	for i in vel:
+		#print ("Applying operator",i)
+		t = SO( tourpos, i )
+		cost = calcTourCost( t )
+		if (cost < min_cost):
+			min_cost = cost
+			min_cost_tour = t.copy()
+
+	return min_cost_tour,min_cost
+
+
+
